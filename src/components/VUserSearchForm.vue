6f2ac8de1309b2efb1b06d068aa2a9e0da3ceb66
clecio <clecioaljinior@gmail.com>
2019-03-27 16:23:19 -0300

    add: project

---
+++ src/components/VUserSearchForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <form @submit.prevent="$emit('submitted', username)">
+      <el-input
+        placeholder="Pesquise o usuario"
+        v-model="username"
+        class="search-form__input">
+
+        <el-button
+          class="search-form__button"
+          native-type="submit"
+          slot="append"
+          icon="el-icon-search">
+        </el-button>
+      </el-input>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserSearchForm',
+  data() {
+    return {
+      username: ''
+    }
+  }
+}
+</script>
